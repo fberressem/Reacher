7fa280640bf8d07189b99da64f6b6286c33645f0
fabian <fberres@uni-mainz.de>
2020-08-03 20:13:44 +0200

    Added optional batch-norm

--- model.py
+++ model.py
@@ -15,9 +15,13 @@ class ActorModel(nn.Module):
 
         input_size = param_dict.get("input_size", 33)
         self.output_size = param_dict.get("output_size", 4)
+        self.batch_norm = param_dict.get("batch_norm", False)
         hn = param_dict.get("hn", [128, 128, 64, 32])
 
         hn = [input_size] + hn + [self.output_size]
+
+        if self.batch_norm:
+            self.bn = nn.BatchNorm1d(input_size)
 
         self.hidden = nn.ModuleList()
         for k in range(len(hn)-1):
@@ -30,6 +34,10 @@ class ActorModel(nn.Module):
         ======
            x(torch.tensor): current state
         """
+
+        if self.batch_norm:
+            x = self.bn(x)
+
         for k in range(len(self.hidden)-1):
             x = F.relu(self.hidden[k](x))
         x = F.tanh(self.hidden[-1](x))
@@ -50,10 +58,15 @@ class CriticModel(nn.Module):
 
         state_size = param_dict.get("state_size", 33)
         self.action_size = param_dict.get("action_size", 4)
+        self.batch_norm = param_dict.get("batch_norm", False)
         hn = param_dict.get("hn", [128, 128, 64, 32])
         self.concat_stage = param_dict.get("concat_stage", 0)   # parameter to set at which state the action should be concatenated
 
         hn = [state_size] + hn + [1]
+
+        if self.batch_norm:
+            self.bn_state = nn.BatchNorm1d(state_size)
+            self.bn_action = nn.BatchNorm1d(self.action_size)
 
         self.hidden = nn.ModuleList()
         for k in range(len(hn)-1):
@@ -68,6 +81,10 @@ class CriticModel(nn.Module):
            state(torch.tensor): current state
            action(torch.tensor): proposed action
         """
+        if self.batch_norm:
+            state = self.bn_state(state)
+            action = self.bn_action(action)
+
         x = state
 
         for k in range(0, len(self.hidden)-1):
